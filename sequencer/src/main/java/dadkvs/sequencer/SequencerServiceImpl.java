d28e24214f672ca8c2a8f6beb13b782512b25ccc
pereira0x <jose.a.pereira03@gmail.com>
2024-09-17 11:42:56 +0100

    feat: add sequencer

---
+++ sequencer/src/main/java/dadkvs/sequencer/SequencerServiceImpl.java
@@ -0,0 +1,26 @@
+package dadkvs.sequencer;
+
+import io.grpc.stub.StreamObserver;
+
+import dadkvs.DadkvsSequencer;
+import dadkvs.DadkvsSequencer.GetSeqNumberResponse;
+import dadkvs.DadkvsSequencerServiceGrpc;
+
+public class SequencerServiceImpl extends DadkvsSequencerServiceGrpc.DadkvsSequencerServiceImplBase {
+
+  int seqNumber;
+
+  public SequencerServiceImpl() {
+    seqNumber = 0;
+  }2
+
+  @Override
+  public synchronized void getSeqNumber(
+      DadkvsSequencer.GetSeqNumberRequest request, StreamObserver<DadkvsSequencer.GetSeqNumberResponse> responseObserver) {
+    seqNumber++;
+    DadkvsSequencer.GetSeqNumberResponse response =
+        GetSeqNumberResponse.newBuilder().setSeqNumber(seqNumber).build();
+    responseObserver.onNext(response);
+    responseObserver.onCompleted();
+  }
+}
