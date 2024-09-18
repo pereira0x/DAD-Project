5e483bf4589722a6f8a2eff62123bbd4d7c0b8b5
pedro.a.curto@tecnico.ulisboa.pt <pedroalexandrecurto@gmail.com>
2024-09-18 16:55:21 +0100

    feat: add sequencer startup to shell script

--- server/sv_start.sh
+++ server/sv_start.sh
@@ -12,6 +12,7 @@ cleanup() {
     port=$((base_port + i))
     fuser -k ${port}/tcp
   done
+  fuser -k 8090/tcp
   for pid in "${pids[@]}"
   do
     kill $pid
@@ -23,10 +24,22 @@ trap cleanup SIGINT
 # initial cleanup to kill any servers that might be running
 cleanup
 
+# initialize sequencer
+cd ../sequencer
+mvn compile exec:java | ts '%Y-%m-%d %H:%M:%S' > "../server/logs/sequencer.log" &
+pids+=($!)
+
+# awaits for the string "Sequencer server started" in the sequencer log to start the servers
+cd ../server
+while ! grep -q "Sequencer server started" logs/sequencer.log
+do
+  sleep 1
+done
+
 for i in $(seq 0 $((num_servers - 1)))
 do
   echo "Starting server $i"
-  mvn exec:java -Dexec.args="$base_port $i" | ts '%Y-%m-%d %H:%M:%S' > "logs/server_${base_port}_${i}.log" 2>&1 &
+  mvn compile exec:java -Dexec.args="$base_port $i" | ts '%Y-%m-%d %H:%M:%S' > "logs/server_${base_port}_${i}.log" 2>&1 &
   pids+=($!)
 done
 
