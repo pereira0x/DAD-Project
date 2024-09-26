37de1470af9bd20490043c531a8189db133bfff4
pedro.a.curto@tecnico.ulisboa.pt <pedroalexandrecurto@gmail.com>
2024-09-26 11:43:38 +0100

    feat: add custom debug class

--- server/src/main/java/dadkvs/server/DadkvsServer.java
+++ server/src/main/java/dadkvs/server/DadkvsServer.java
@@ -14,6 +14,13 @@ public class DadkvsServer {
 								    
     /** Server host port. */
     private static int port;
+
+	public static void debug(String debugClass, String format, Object... args) {
+		// TODO -- add check to debug mode variable
+		System.out.printf("[%s] ", debugClass);
+		System.out.printf(format, args);
+		System.out.println();
+	}
     
     public static void main(String[] args) throws Exception {
 	final int kvsize = 1000;
