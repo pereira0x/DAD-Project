013c9618622f5bccdccda9064e9d812235fed753
Simão Sanguinho <simao.sanguinho@tecnico.ulisboa.pt>
2024-10-04 07:27:30 +0100

    feat: add collectors teacher's update

--- util/src/main/java/dadkvs/util/GenericResponseCollector.java
+++ util/src/main/java/dadkvs/util/GenericResponseCollector.java
@@ -2,36 +2,40 @@ package dadkvs.util;
 
 import java.util.ArrayList;
 
-public class GenericResponseCollector<T>  {
-    ArrayList<T> collectedResponses;
-    int   received;
-    int   pending;
+public class GenericResponseCollector<T> {
+    ArrayList<T> collected_responses;
+    int received;
+    int pending;
+    boolean target_reached;
 
     public GenericResponseCollector(ArrayList<T> responses, int maxresponses) {
-        collectedResponses = responses;
-	received = 0;
-	pending = maxresponses;
+        collected_responses = responses;
+        received = 0;
+        pending = maxresponses;
+        target_reached = false;
     }
 
     synchronized public void addResponse(T resp) {
-        collectedResponses.add(resp);
-	received++;
-	pending--;
-	notifyAll();
+        if (!target_reached) {
+            collected_responses.add(resp);
+        }
+        received++;
+        pending--;
+        notifyAll();
     }
 
     synchronized public void addNoResponse() {
-	pending--;
-	notifyAll();
+        pending--;
+        notifyAll();
     }
- 
+
     synchronized public void waitForTarget(int target) {
         while ((pending > 0) && (received < target)) {
             try {
-		wait ();
-	    }
-	    catch (InterruptedException e) {
-	    }
-	}
+                wait();
+            } catch (InterruptedException e) {
+            }
+            target_reached = true;
+        }
     }
 }
