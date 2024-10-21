c3ac4782e5c468aac4df6244d44ee94d18be6436
Pedro Curto <pedroalexandrecurto@gmail.com>
2024-10-21 16:45:55 +0100

    chore: remove timestamp from logs to simplify reading

--- server/sv_start.sh
+++ server/sv_start.sh
@@ -26,7 +26,8 @@ cleanup
 for i in $(seq 0 $((num_servers - 1)))
 do
   echo "Starting server $i"
-  mvn compile exec:java -Dexec.args="$base_port $i" | ts '%Y-%m-%d %H:%M:%S' > "logs/server_${base_port}_${i}.log" 2>&1 &
+  #mvn compile exec:java -Dexec.args="$base_port $i" | ts '%Y-%m-%d %H:%M:%S' > "logs/server_${base_port}_${i}.log" 2>&1 &
+  mvn compile exec:java -Dexec.args="$base_port $i" > "logs/server_${base_port}_${i}.log" 2>&1 &
   pids+=($!)
 done
 
